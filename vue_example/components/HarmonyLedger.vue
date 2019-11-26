e348297dae8e413da97c32c048165294304b0b7a
coolcottontail <coolcottontail@gmail.com>
2019-11-25 20:58:39 -0800

    working sendTxn but failing stakingTxn.encode

--- vue_example/components/HarmonyLedger.vue
+++ vue_example/components/HarmonyLedger.vue
@@ -52,16 +52,10 @@
 import TransportWebUSB from '@ledgerhq/hw-transport-webusb';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import TransportU2F from '@ledgerhq/hw-transport-u2f';
-import {
-    arrayify, hexlify, stripZeros, encode,
-} from '@harmony-js/crypto';
 import HarmonyApp from '../../src';
 
 const { Harmony } = require('@harmony-js/core');
 const {
-    Description,
-    Decimal,
-    CommissionRate,
     StakingTransaction,
     CreateValidator,
 } = require('@harmony-js/staking');
@@ -170,53 +164,26 @@ export default {
             // Given a transport (U2F/HIF/WebUSB) it is possible instantiate the app
             const transport = await this.getTransport();
             const app = new HarmonyApp(transport);
-            let response = await app.publicKey(true);
-            if (response.return_code !== 0x9000) {
-                this.log(`Error [${response.return_code}] ${response.error_message}`);
-                return;
-            }
-            this.log('Harmony one address for ledger is:');
-            this.log(response.one_address.toString());
-            const accountNonce = await HarmonyApp.getAccountShardNonce(
-                response.one_address.toString(), 0, harmony.messenger,
-            );
+
+            const shardId = 0;
             const txn = harmony.transactions.newTx({
                 //  token send to
                 to: 'one1sl6hku7wxgxnhajrc0a96p6zpea6qr0p0sqajk',
-                nonce: accountNonce,
+                nonce: 0,
                 // amount to send
                 value: '100000000000000000',
                 // gas limit, you can use string
                 gasLimit: '210000',
-                shardID: 0,
+                shardID: shardId,
                 toShardID: 1,
                 gasPrice: new harmony.utils.Unit('100').asGwei().toWei(),
             });
 
-            const [unsignedRawTransaction, raw] = txn.getRLPUnsigned();
-            response = await app.signTx(unsignedRawTransaction);
-            const bytes = response.signature;
-            const r = hexlify(bytes.slice(0, 32));
-            const s = hexlify(bytes.slice(32, 64));
-            let v = bytes[64];
-            if (v !== 27 && v !== 28) {
-                v = 27 + (v % 2);
-            }
-            // const signed = txn.getRLPSigned(raw, signature);
-            // replace empty r,s,v with signature r,s,v
-            raw.pop();
-            raw.pop();
-            raw.pop();
-            v += harmony.chainId * 2 + 8;
-            raw.push(hexlify(v));
-            raw.push(stripZeros(arrayify(r) || []));
-            raw.push(stripZeros(arrayify(s) || []));
-
-            const encodedRaw = encode(raw);
-            txn.setParams({ ...txn.txParams, rawTransaction: encodedRaw });
+            const signedTxn = await app.signTransaction(txn,
+                harmony.chainId, shardId, harmony.messenger);
 
             // Frontend received back the signedTxn and do the followings to Send transaction.
-            txn.observed()
+            signedTxn.observed()
                 .on('transactionHash', (txnHash) => {
                     console.log('');
                     console.log('--- hash ---');
@@ -251,7 +218,7 @@ export default {
 
             // send the txn, get [Transaction, transactionHash] as result
 
-            const [sentTxn, txnHash] = await txn.sendTransaction();
+            const [sentTxn, txnHash] = await signedTxn.sendTransaction();
 
             // to confirm the result if it is already there
 
@@ -277,6 +244,7 @@ export default {
                 process.exit();
             }
         },
+
         async signStake() {
             this.deviceLog = [];
             const transport = await this.getTransport();
@@ -285,125 +253,41 @@ export default {
             const shardStructure = await harmony.blockchain.getShardingStructure();
             harmony.shardingStructures(shardStructure.result);
 
-            let response = await app.publicKey(true);
-            if (response.return_code !== 0x9000) {
-                this.log(`Error [${response.return_code}] ${response.error_message}`);
-                return;
-            }
-            const address = response.one_address.toString();
-
-            this.log('Harmony one address for ledger is:');
-            this.log(address);
-            const accountNonce = await HarmonyApp.getAccountShardNonce(
-                address, 0, harmony.messenger,
-            );
-
-            const desc = new Description('Alice', 'alice', 'alice.harmony.one', 'Bob', "Don't mess with me!!!");
-            const rate = new Decimal('0.1');
-            const max = new Decimal('0.9');
-            const change = new Decimal('0.05');
-
-            const commission = new CommissionRate(rate, max, change);
-            const createMsg = new CreateValidator(
-                'one1q6gkzcap0uruuu8r6sldxuu47pd4ww9w9t7tg6',
-                desc,
-                commission,
-                '0x8AC7230489E80000',
-                '0xA2A15D09519BE00000',
-                [
+            const createMsg = new CreateValidator({
+                validatorAddress: 'one1q6gkzcap0uruuu8r6sldxuu47pd4ww9w9t7tg6',
+                description: {
+                    name: 'Alice',
+                    identity: 'alice',
+                    website: 'alice.harmony.one',
+                    securityContact: 'Bob',
+                    details: "Don't mess with me!!!",
+                },
+                commissionRates: {
+                    rate: '0.1',
+                    maxRate: '0.9',
+                    maxChangeRate: '0.05',
+                },
+                minSelfDelegation: '0xa',
+                maxTotalDelegation: '0x0bb8',
+                slotPubKeys: [
                     '0xb9486167ab9087ab818dc4ce026edb5bf216863364c32e42df2af03c5ced1ad181e7d12f0e6dd5307a73b62247608611',
                 ],
-                '0x56BC75E2D63100000', // 0x56BC75E2D63100000
-            );
+                amount: '0x64',
+            });
 
-            const stakingTxn = new StakingTransaction('0x', createMsg, 0, '0x', '0x0927c0', 2, 2, '', '', harmony.messenger);
-            stakingTxn.setNonce(accountNonce);
-            stakingTxn.setFromAddress(address);
+            const shardId = 0;
+            const stakingTxn = new StakingTransaction({
+                directive: '0x',
+                stakeMsg: createMsg,
+                nonce: '0',
+                gasPrice: new harmony.utils.Unit('100').asGwei().toWei(),
+                gasLimit: '510000',
+                chainId: 0,
+                from: 'one1q6gkzcap0uruuu8r6sldxuu47pd4ww9w9t7tg6',
+            });
 
-            const [unsignedRawTransaction, raw] = stakingTxn.encode();
-            stakingTxn.setUnsigned(unsignedRawTransaction);
-
-            console.log(unsignedRawTransaction);
-            response = await app.signStake(unsignedRawTransaction);
-            console.log('signature = ', response.signature.toString('hex'));
-            const bytes = response.signature;
-            const r = hexlify(bytes.slice(0, 32));
-            const s = hexlify(bytes.slice(32, 64));
-            let v = bytes[64];
-            if (v !== 27 && v !== 28) {
-                v = 27 + (v % 2);
-            }
-            // const signed = txn.getRLPSigned(raw, signature);
-            console.log(r, s, v);
-            // replace empty r,s,v with signature r,s,v
-            raw.pop();
-            raw.pop();
-            raw.pop();
-            v += harmony.chainId * 2 + 8;
-            raw.push(hexlify(v));
-            raw.push(stripZeros(arrayify(r) || []));
-            raw.push(stripZeros(arrayify(s) || []));
-            console.log('v=', v, 's=', s, 'r=', r);
-            const encodedRaw = encode(raw);
-            console.log('encoded raw is:');
-            console.log(encodedRaw);
-            stakingTxn.setRawTransaction(encodedRaw);
-            console.log(stakingTxn);
-
-            stakingTxn.observed()
-                .on('transactionHash', (txnHash) => {
-                    console.log('');
-                    console.log('--- hash ---');
-                    console.log('');
-                    console.log(txnHash);
-                    console.log('');
-                })
-                .on('receipt', (receipt) => {
-                    console.log('');
-                    console.log('--- receipt ---');
-                    console.log('');
-                    console.log(receipt);
-                    console.log('');
-                })
-                .on('cxReceipt', (receipt) => {
-                    console.log('');
-                    console.log('--- cxReceipt ---');
-                    console.log('');
-                    console.log(receipt);
-                    console.log('');
-                })
-                .on('error', (error) => {
-                    console.log('');
-                    console.log('--- error ---');
-                    console.log('');
-                    console.log(error);
-                    console.log('');
-                });
-
-            // console.log(signedTxn);
-            const [sentTxn, txnHash] = await stakingTxn.sendTransaction();
-
-            const confiremdTxn = await sentTxn.confirm(txnHash);
-
-            // if the transactino is cross-shard transaction
-            if (!confiremdTxn.isCrossShard()) {
-                if (confiremdTxn.isConfirmed()) {
-                    console.log('--- Result ---');
-                    console.log('');
-                    console.log('Normal transaction');
-                    console.log(`${txnHash} is confirmed`);
-                    console.log('');
-                    process.exit();
-                }
-            }
-            if (confiremdTxn.isConfirmed() && confiremdTxn.isCxConfirmed()) {
-                console.log('--- Result ---');
-                console.log('');
-                console.log('Cross-Shard transaction');
-                console.log(`${txnHash} is confirmed`);
-                console.log('');
-                process.exit();
-            }
+            console.log(stakingTxn.encode());
+            console.log(app, stakingTxn, shardId);
         },
     },
 };
